231454c7860647f439fefc407b025c32c78fb98b
caspianX51 <57324921+caspianX51@users.noreply.github.com>
2022-01-30 18:48:12 -0600

    added the thingy

---
+++ howmuchtime.js
@@ -0,0 +1,10 @@
+                var ayer = 1850;
+                var hoy = 2022;
+
+                function substraction(restado, resta){
+                 return restado - resta;
+                }
+
+                var tiempo = substraction(hoy, ayer);
+
+                console.log(tiempo);
