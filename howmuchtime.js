0483983548611a4425ba246865d1252124ef752a
AlOs <alanosmarvira@gmail.com>
2022-02-06 12:24:51 -0600

    added logevity calc

--- howmuchtime.js
+++ howmuchtime.js
@@ -1,10 +1,22 @@
-                var ayer = 1850;
-                var hoy = 2022;
+var ayer = 2002;
+var hoy = 2022;
 
-                function substraction(restado, resta){
-                 return restado - resta;
-                }
+function substraction(restado, resta){
+ return restado - resta;
+}
 
-                var tiempo = substraction(hoy, ayer);
+var tiempo = substraction(hoy, ayer);
 
-                console.log(tiempo);
+console.log(tiempo);
+
+function longevidad(anios){
+    if (anios > 100) {
+        return "mas de un siglo";
+    } else if (anios > 10) {
+        return "mas de una decada";
+    } else { 
+        return "menos de una decada"
+    }
+}
+
+console.log(longevidad(tiempo))
